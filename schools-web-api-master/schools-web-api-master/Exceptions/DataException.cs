f06b46899ed44c3d9614fcc3f642d3f88e9f562f
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2024-12-16 22:01:45 +0100

    Delete old project

--- schools-web-api-master/schools-web-api-master/Exceptions/DataException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace schools_web_api.TokenManager.Exceptions
-{
-    public class DataException : Exception
-    {
-        public DataException(string message) : base($"Data error: {message}") { }
-    }
-}
