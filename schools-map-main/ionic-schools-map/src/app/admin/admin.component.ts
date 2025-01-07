641b9e605c43810d6e1ed70a998a20ca6dfeaa89
mateuszk5 <mateuszkisiel6@gmail.com>
2025-01-07 15:32:32 +0100

    moved school view from home to admin panel

--- schools-map-main/ionic-schools-map/src/app/admin/admin.component.ts
+++ schools-map-main/ionic-schools-map/src/app/admin/admin.component.ts
@@ -4,6 +4,10 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { AdminLoginComponent } from '../admin-login/admin-login.component';
 
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -14,12 +18,68 @@ import { AdminLoginComponent } from '../admin-login/admin-login.component';
 export class AdminComponent  implements OnInit {
   isLoggedIn = false;
 
-  constructor() { }
+  private oldSchoolsUrl = 'http://localhost:5000/api/rspo/old-schools';
+  private newSchoolsUrl = 'http://localhost:5000/api/rspo/new-schools';
+
+  filteredSchools: any[] = [];
+  oldSchools: any[] = [];
+  newSchools: any[] = [];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {}
 
   onLoginSuccess() {
     this.isLoggedIn = true;
+    this.loadSchools();
+  }
+
+    getSchools(): Observable<{ oldSchools: any[], newSchools: any[] }> {
+      return forkJoin({
+        oldSchools: this.http.get<any[]>(this.oldSchoolsUrl),
+        newSchools: this.http.get<any[]>(this.newSchoolsUrl)
+      });
+    }
+
+  public loadSchools() {
+    this.getSchools().subscribe(response => {
+      // Process oldSchools
+      this.oldSchools = response.oldSchools.map(school => ({
+        ...school,
+        isExpanded: false,
+        isOldObj: true // Mark explicitly
+      }));
+      
+      // Process newSchools
+      this.newSchools = response.newSchools.map(school => ({
+        ...school,
+        isExpanded: false,
+        isNewObj: true, // Mark explicitly
+        matchedOldSchool: null // Prepare for comparison
+      }));
+    });
+  }
+  
+
+  public toggleDetails(school: any) {
+    school.isExpanded = !school.isExpanded;
+  
+    if (school.isExpanded && school.isNewObj) {
+      // Find a matching old school
+      school.matchedOldSchool = this.oldSchools.find(
+        oldSchool => oldSchool.nazwa === school.nazwa
+      );
+    }
+  }
+
+  public compareValues(newVal: any, oldVal: any): string {
+    if (newVal === oldVal) {
+      return 'green';  // Same value
+    } else if (!newVal || !oldVal) {
+      return '';  // If one of the values is 'N/A' or empty, no highlight
+    } else {
+      return 'red';  // Different values
+    }
   }
 
 }
--- schools-map-main/ionic-schools-map/src/app/main/main.component.ts
+++ schools-map-main/ionic-schools-map/src/app/main/main.component.ts
@@ -3,14 +3,6 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, forkJoin } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root'
-})
-
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -69,26 +61,12 @@ export class MainComponent implements OnInit {
     { value: 'Technikum', label: 'Technikum' }
   ];
 
-  private oldSchoolsUrl = 'http://localhost:5000/api/rspo/old-schools';
-  private newSchoolsUrl = 'http://localhost:5000/api/rspo/new-schools';
-
   filteredSchools: any[] = [];
-  oldSchools: any[] = [];
-  newSchools: any[] = [];
-  schools: any[] = [];
 
-  getSchools(): Observable<{ oldSchools: any[], newSchools: any[] }> {
-    return forkJoin({
-      oldSchools: this.http.get<any[]>(this.oldSchoolsUrl),
-      newSchools: this.http.get<any[]>(this.newSchoolsUrl)
-    });
-  }
+  constructor() { }
 
-  constructor(private http: HttpClient) { }
-  
   ngOnInit(): void {
     this.setupEventListeners();
-    this.loadSchools();
   }
 
   public setupEventListeners() {
@@ -116,47 +94,6 @@ export class MainComponent implements OnInit {
     this.hamburgerElement.nativeElement.classList.toggle('change-bar1');
     this.hamburgerElement.nativeElement.classList.toggle('change-bar2');
     this.hamburgerElement.nativeElement.classList.toggle('change-bar3');
-  }
-
-  public loadSchools() {
-    this.getSchools().subscribe(response => {
-      // Process oldSchools
-      this.oldSchools = response.oldSchools.map(school => ({
-        ...school,
-        isExpanded: false,
-        isOldObj: true // Mark explicitly
-      }));
-      
-      // Process newSchools
-      this.newSchools = response.newSchools.map(school => ({
-        ...school,
-        isExpanded: false,
-        isNewObj: true, // Mark explicitly
-        matchedOldSchool: null // Prepare for comparison
-      }));
-    });
-  }
-  
-
-  public toggleDetails(school: any) {
-    school.isExpanded = !school.isExpanded;
-  
-    if (school.isExpanded && school.isNewObj) {
-      // Find a matching old school
-      school.matchedOldSchool = this.oldSchools.find(
-        oldSchool => oldSchool.nazwa === school.nazwa
-      );
-    }
-  }
-
-  public compareValues(newVal: any, oldVal: any): string {
-    if (newVal === oldVal) {
-      return 'green';  // Same value
-    } else if (!newVal || !oldVal) {
-      return '';  // If one of the values is 'N/A' or empty, no highlight
-    } else {
-      return 'red';  // Different values
-    }
   }
 
   public filterSchools() {
